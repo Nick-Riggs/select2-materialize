2bdacb1948b9f34f710d065ddd3da5c6b92fb711
Nick Riggs <nicholasriggs@gmail.com>
2016-07-12 11:49:51 -0500

    change the selection to include dynamically added controls

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    $("select").on("select2:open", function() {
+    $(document).on("select2:open", "select", function() {
         var $parent = $(this).parent();
 
         $parent.children("label").addClass("active").addClass("focus");
         $parent.find(".select2-selection").addClass("focus");
     });
 
-    $("select").on("select2:close", function() {
+    $(document).on("select2:close", "select", function() {
         if ($(this).children(":selected").length) {
             return;
         }
